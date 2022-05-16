761c3032d73e1b82267c406200d0c12e948ed59c
Оксана Анатольевна Лозовская <aptstudent@mail.ru>
2022-05-16 19:03:41 +1000

    Добавьте файлы проекта.

---
+++ DockPanel2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _3._5DockPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для DockPanel2.xaml
+    /// </summary>
+    public partial class DockPanel2 : Window
+    {
+        public DockPanel2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3._5DockPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DockPanel1 dockPanel1 = new DockPanel1();
+            dockPanel1.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DockPanel2 dockPanel2 = new DockPanel2();
+            dockPanel2.Show();
+        }
+    }
+}
